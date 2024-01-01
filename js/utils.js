7002ebd31ea2f2bb2ce44666581a8f6cadffd9f2
aymar99 <lramya26031999@gmail.com>
2024-01-01 16:29:22 +0530

    feature scroll progress indicator impl

--- js/utils.js
+++ js/utils.js
@@ -5,3 +5,16 @@ function getDateDifferenceBetweenCurrentAndGivenDate(dateString) {
   let daysDifference = timeDifference / (1000 * 60 * 60 * 24);
   return daysDifference;
 }
+
+const scrollProgress = document.getElementById("scroll-progress");
+const height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll", () => {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  scrollProgress.style.width = `${scrolled}%`;
+});
